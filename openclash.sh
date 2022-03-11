56fc77237ea8f4217817960ac62e47f7125cafb1
Haris131 <41672454+Haris131@users.noreply.github.com>
2022-03-11 18:19:01 +0700

    Add files via upload

--- openclash.sh
+++ openclash.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 clear
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+pack(){
+ echo -e $b">"$w" prepare for installing dependencies ..."
+ if [ -f $(pkg list-installed 2>/dev/null|grep unzip|awk '{print $1}') ]; then
+  echo -e $b">"$w" installing package: "$g"unzip"$w
+  pkg install unzip -y
+ fi
+ if [ -f $(pkg list-installed 2>/dev/null|grep screen|awk '{print $1}') ]; then
+  echo -e $b">"$w" installing package: "$g"screen"$w
+  pkg install screen -y
+ fi
+ if [ -f $(pkg list-installed 2>/dev/null|grep clash|awk '{print $1}') ]; then
+  echo -e $b">"$w" installing package: "$g"clash"$w
+  pkg install clash -y
+ fi
+ echo -e $b">"$w" successfully installing dependencies"
+ rm -rf $HOME/.config/clash*
+ curl -sLO "https://github.com/Haris131/clash/blob/main/.config.zip"
+ echo -e $b">"$W" settings clash"
+ unzip .config.zip -C $HOME/.config/clash >&/dev/null
+ rm -rf .config.zip*
+ exit
+}
 
 case $1 in
  "start")
@@ -27,6 +53,9 @@ case $1 in
  "akun")
  nano $HOME/.config/clash/universal.yaml
  exit
+ ;;
+ "install")
+ pack;exit
  ;;
 esac
 echo -e "\e[38;3m Start Clash\e[0m \e[34;1m(openclash start)\e[0m"
